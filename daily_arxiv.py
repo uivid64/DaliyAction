8fb89e30ade99f48a54f4f54498fa12ad732c53d
David <70085263+uivid64@users.noreply.github.com>
2022-08-28 15:57:14 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -186,11 +186,11 @@ if __name__ == "__main__":
     data_collector_web= []
     
     keywords = dict()
-    keywords["SLAM"]                = "SLAM"
-    keywords["SFM"]                 = "SFM"+"OR"+"\"Structure from Motion\""
-    keywords["Visual Localization"] = "\"Camera Localization\"OR\"Visual Localization\"OR\"Camera Re-localisation\"OR\"Loop Closure Detection\"OR\"visual place recognition\"OR\"image retrieval\""
-    keywords["Keypoint Detection"]  = "\"Keypoint Detection\"OR\"Feature Descriptor\""
-    keywords["Image Matching"]      = "\"Image Matching\"OR\"Keypoint Matching\""
+    keywords["Gradient Compression"]                = "Gradient Compression"
+    keywords["Distributed Training"]                 = "Distributed Training"+"OR"+"\"Structure from Motion\""
+    keywords["Distributed Deep Learning"] = "\"Distributed Deep Learning\"OR\"Distributed Systems\"OR\"Communication in Distributed Deep Learning\"OR\"Parallel Training\"OR\"distributed training\""
+    # keywords["Keypoint Detection"]  = "\"Keypoint Detection\"OR\"Feature Descriptor\""
+    # keywords["Image Matching"]      = "\"Image Matching\"OR\"Keypoint Matching\""
 
     for topic,keyword in keywords.items():
  
